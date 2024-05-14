7d8cb8427e9e55ef4b8f83545cf594c97c1b20a3
maksp <55075653+maksp86@users.noreply.github.com>
2024-05-14 21:14:17 +0500

    front updates

--- webapp/src/pages/Home.jsx
+++ webapp/src/pages/Home.jsx
@@ -1,49 +1,91 @@
-import { Container, Row, Col, ProgressBar, Navbar } from "react-bootstrap"
-import { HomeFontIcon, HomeSVGIcon } from "@react-md/material-icons"
+import { Container, Row, Col, ProgressBar, Navbar, ButtonGroup, Button } from "react-bootstrap"
+import { HouseDoor, Person } from "react-bootstrap-icons"
 
-import './Home.css'
+// import './Home.scss'
+import './Navigation.css'
+
+function NavButton(props) {
+    const active = true
+
+    return (
+        <Button
+            variant="dark"
+            className={"navigation-button " + (active ? "navigation-button-active " : "") + props.className}
+            onClick={() => navigateTo(props.path)}
+        >
+            <div className="navigation-selector-holder">
+                {props.children}
+                <span className="fw-medium">{props.text}</span>
+            </div>
+        </Button>
+    )
+}
 
 function Home() {
     let user = "eblan"
     return (
         <>
-            <Container id="main" fluid="md" className="mt-5">
+            <Container id="main" fluid="sm" className="mt-5 my-1">
                 <Row>
-                    <Col className="align-items-left">
+                    <Col xs="12" md="6" className="align-items-left">
                         <h1>hello, {user}</h1>
                     </Col>
                     <Col className="align-self-center">
-                        <Row className="align-items-center justify-content-center">
-                            <Col className="col-8">
+                        <Row className="align-items-center">
+                            <Col>
                                 <ProgressBar now={60} />
                             </Col>
-                            <Col className="col-4">
-                                <h5 className="m-0">1000px</h5>
+                            <Col xs="auto">
+                                <h5 className="m-0 text-end">1000px</h5>
                             </Col>
                         </Row>
                     </Col>
                 </Row>
-            </Container>
-            <Container fluid="sm" className="fixed-bottom text-center navigation">
-                <Row className="justify-content-md-center">
-                    <Col className="navchild">
-                        <HomeSVGIcon />
-                        <p className="m-0">text</p>
+                <Row className="align-items-center">
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
                     </Col>
-                    <Col className="navchild">
-                        <HomeSVGIcon />
-                        <p className="m-0">text</p>
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
                     </Col>
-                    <Col className="navchild">
-                        <HomeSVGIcon />
-                        <p className="m-0">text</p>
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
                     </Col>
-                    <Col className="navchild">
-                        <HomeSVGIcon />
-                        <p className="m-0">text</p>
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
+                    </Col>
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
+                    </Col>
+                    <Col className="g-2">
+                        <div style={{ height: "200px", width: "400px", backgroundColor: "black" }}></div>
                     </Col>
                 </Row>
             </Container>
+            <Navbar
+                bg="dark"
+                variant="dark"
+                fixed="bottom"
+                className="navigation p-0 mx-auto">
+
+                <ButtonGroup className="navigation-selectors">
+                    <NavButton path="/" exact={true} text="1">
+                        <HouseDoor size={30} />
+                    </NavButton>
+
+                    <NavButton path="/2" text="2">
+                        <HouseDoor size={30} />
+                    </NavButton>
+
+                    <NavButton path="/3" text="3">
+                        <HouseDoor size={30} />
+                    </NavButton>
+
+                    <NavButton path="/4" text="4">
+                        <Person size={30} />
+                    </NavButton>
+                </ButtonGroup>
+            </Navbar >
         </>
     )
 }
