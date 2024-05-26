d5060f2fb378713a6212929de46ce12786e3e8a6
maksp <55075653+maksp86@users.noreply.github.com>
2024-05-27 01:41:17 +0500

    Add photo crop

--- webapp/src/pages/Login.jsx
+++ webapp/src/pages/Login.jsx
@@ -67,90 +67,97 @@ function LoginPage() {
 
     return (
         <>
-            <Container style={{ maxWidth: "50%" }}>
-                <Row>
-                    <Col>
-                        <h2 className="mx-2 my-4">{registerMode ? "Register account" : "Login"}</h2>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <FloatingLabel
-                            controlId="usernameInput"
-                            label="Username"
-                            className="mb-3">
-                            <Form.Control
-                                disabled={api.busy}
-                                type="username"
-                                placeholder="user"
-                                value={formData.username || ""}
-                                onChange={(e) => setField("username", e.target.value)}
-                                isInvalid={!!errors.username} />
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.username}
-                            </Form.Control.Feedback>
-                        </FloatingLabel>
-
-                        {
-                            registerMode &&
-                            <FloatingLabel controlId="nameInput" label="Name" className="mb-3">
+            <Row className="justify-content-center">
+                <Col
+                    sm="11" lg="6"
+                    className="card p-4"
+                    style={{
+                        borderRadius: "20px"
+                    }}>
+                    <Row>
+                        <Col>
+                            <h2 className="mx-2 mb-4">{registerMode ? "Register account" : "Login"}</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <FloatingLabel
+                                controlId="usernameInput"
+                                label="Username"
+                                className="mb-3">
                                 <Form.Control
                                     disabled={api.busy}
-                                    type="name"
-                                    placeholder="Ivan Ivanov"
-                                    value={formData.name || ""}
-                                    onChange={(e) => setField("name", e.target.value)}
-                                    isInvalid={!!errors.name} />
+                                    type="username"
+                                    placeholder="user"
+                                    value={formData.username || ""}
+                                    onChange={(e) => setField("username", e.target.value)}
+                                    isInvalid={!!errors.username} />
                                 <Form.Control.Feedback type='invalid'>
-                                    {errors.name}
+                                    {errors.username}
                                 </Form.Control.Feedback>
                             </FloatingLabel>
-                        }
 
-                        <FloatingLabel controlId="passwordInput" label="Password" className="mb-3">
-                            <Form.Control
-                                disabled={api.busy}
-                                type={showPassword ? "text" : "password"}
-                                placeholder="Password"
-                                value={formData.password || ""}
-                                onChange={(e) => setField("password", e.target.value)}
-                                isInvalid={!!errors.password} />
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.password}
-                            </Form.Control.Feedback>
-                        </FloatingLabel>
-                        <Row className="justify-content-end">
-                            <Col xs="auto">
-                                <Switch
-                                    reverse
-                                    label="Show password"
-                                    className="mx-2"
-                                    checked={showPassword}
-                                    onChange={
-                                        (e) => setShowPassword(e)
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                        <Row className="text-center">
-                            <h6 className="text-danger">{errors.summary}</h6>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row className="mt-3 px-3 justify-content-end align-items-center">
-                    <Col className="align-items-center">
-                        {!registerMode &&
-                            <a href="#" onClick={() => setRegisterMode(true)}>Want to register?</a>
-                        }
-                    </Col>
-                    <Col xs="4" className="d-grid">
-                        <Button variant="primary" type="button" disabled={api.busy}
-                            onClick={() => { processAuth() }}>
-                            {registerMode ? "Register" : "Login"}
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
+                            {
+                                registerMode &&
+                                <FloatingLabel controlId="nameInput" label="Name" className="mb-3">
+                                    <Form.Control
+                                        disabled={api.busy}
+                                        type="name"
+                                        placeholder="Ivan Ivanov"
+                                        value={formData.name || ""}
+                                        onChange={(e) => setField("name", e.target.value)}
+                                        isInvalid={!!errors.name} />
+                                    <Form.Control.Feedback type='invalid'>
+                                        {errors.name}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                            }
+
+                            <FloatingLabel controlId="passwordInput" label="Password" className="mb-3">
+                                <Form.Control
+                                    disabled={api.busy}
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Password"
+                                    value={formData.password || ""}
+                                    onChange={(e) => setField("password", e.target.value)}
+                                    isInvalid={!!errors.password} />
+                                <Form.Control.Feedback type='invalid'>
+                                    {errors.password}
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                            <Row className="justify-content-end">
+                                <Col xs="auto">
+                                    <Switch
+                                        reverse
+                                        label="Show password"
+                                        className="mx-2"
+                                        checked={showPassword}
+                                        onChange={
+                                            (e) => setShowPassword(e)
+                                        }
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="text-center">
+                                <h6 className="text-danger">{errors.summary}</h6>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3 px-3 justify-content-end align-items-center">
+                        <Col className="align-items-center">
+                            {!registerMode &&
+                                <a href="#" onClick={() => setRegisterMode(true)}>Want to register?</a>
+                            }
+                        </Col>
+                        <Col xs="4" className="d-grid">
+                            <Button variant="primary" type="button" disabled={api.busy}
+                                onClick={() => { processAuth() }}>
+                                {registerMode ? "Register" : "Login"}
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </>
     )
 }
--- webapp/src/pages/modals/UserEditModal.jsx
+++ webapp/src/pages/modals/UserEditModal.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { Container, Row, Col, ProgressBar, Button, Image, FloatingLabel, Form, Modal } from "react-bootstrap"
+import Cropper from 'react-easy-crop'
 import { ApiContext } from "../../context/api.context";
 import { UserContext } from "../../context/user.context";
 import getErrorMessage from "../../extras/getErrorMessage";
@@ -12,7 +13,11 @@ function UserEditModal() {
 
     const [formData, setFormData] = useState({ name: userContext.user.name, bio: userContext.user.bio })
     const [errors, setErrors] = useState({})
-    const [image, setImage] = useState()
+
+    const [isImageCrop, setIsImageCrop] = useState(false)
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
 
     const setField = (field, value) => {
         setFormData({
@@ -66,80 +71,152 @@ function UserEditModal() {
         }
     }
 
+    async function getCroppedImg(imageSrc, pixelCrop) {
+        const image = await createImageBitmap(imageSrc)
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        const maxSize = Math.max(image.width, image.height);
+        const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
+
+        canvas.width = safeArea;
+        canvas.height = safeArea;
+
+        ctx.drawImage(
+            image,
+            safeArea / 2 - image.width * 0.5,
+            safeArea / 2 - image.height * 0.5
+        );
+        const data = ctx.getImageData(0, 0, safeArea, safeArea);
+
+        canvas.width = pixelCrop.width;
+        canvas.height = pixelCrop.height;
+
+        ctx.putImageData(
+            data,
+            Math.round(0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x),
+            Math.round(0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y)
+        );
+
+        return canvas;
+    }
+
+    async function OnCropComplete() {
+        setIsImageCrop(false)
+        const cropCanvas = await getCroppedImg(formData.image, croppedAreaPixels)
+        cropCanvas.toBlob((blob) => setField("image", blob))
+    }
+
     return (
         <>
             <Modal.Header closeButton>
-                <Modal.Title>Edit user</Modal.Title>
+                <Modal.Title>{isImageCrop ? "Crop image" : "Edit user"}</Modal.Title>
             </Modal.Header>
             <Container>
-                <Row className="text-center">
-                    <Col>
-                        <FloatingLabel
-                            controlId="nameInput"
-                            label="Name"
-                            className="mb-3">
-                            <Form.Control
-                                disabled={api.busy}
-                                type="name"
-                                placeholder="user"
-                                value={formData.name || ""}
-                                onChange={(e) => setField("name", e.target.value)}
-                                isInvalid={!!errors.name} />
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.name}
-                            </Form.Control.Feedback>
-                        </FloatingLabel>
+                {
+                    !isImageCrop && <>
+                        <Row className="text-center">
+                            <Col>
+                                <FloatingLabel
+                                    controlId="nameInput"
+                                    label="Name"
+                                    className="mb-3">
+                                    <Form.Control
+                                        disabled={api.busy}
+                                        type="name"
+                                        placeholder="user"
+                                        value={formData.name || ""}
+                                        onChange={(e) => setField("name", e.target.value)}
+                                        isInvalid={!!errors.name} />
+                                    <Form.Control.Feedback type='invalid'>
+                                        {errors.name}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
 
-                        <FloatingLabel controlId="floatingTextarea2" label="Bio">
-                            <Form.Control
-                                as="textarea"
-                                placeholder="Enter something cool about yourself"
-                                style={{ minHeight: '150px', maxHeight: '300px' }}
-                                rows={2}
-                                maxLength={255}
-                                className="mb-3"
-                                disabled={api.busy}
-                                value={formData.bio || ""}
-                                onChange={(e) => setField("bio", e.target.value)}
-                                isInvalid={!!errors.bio} />
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.bio}
-                            </Form.Control.Feedback>
-                        </FloatingLabel>
-                    </Col>
-                </Row>
-                <Row className="mb-3">
-                    <Col sm="auto">
-                        <Image roundedCircle
-                            style={{ backgroundColor: "#000", width: "10vh", height: "10vh" }} src={image || (userContext.user.image && ("/api/attachments/get?id=" + userContext.user.image))} />
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="formFile" className="mb-3">
-                            <Form.Label>Upload your photo</Form.Label>
-                            <Form.Control
-                                type="file"
-                                accept="image/png, image/jpeg, image/webp"
-                                disabled={api.busy}
-                                onChange={(e) => {
-                                    if (e.target.files && e.target.files.length > 0) {
-                                        setImage(URL.createObjectURL(e.target.files[0]))
-                                        setField("image", e.target.files[0])
-                                    }
-                                }}
-                                isInvalid={!!errors.image}
-                            />
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.image}
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="px-4">
-                    <Button variant="primary" type="button" disabled={api.busy}
-                        onClick={() => processEdit()}>
-                        Save
-                    </Button>
-                </Row>
+                                <FloatingLabel controlId="floatingTextarea2" label="Bio">
+                                    <Form.Control
+                                        as="textarea"
+                                        placeholder="Enter something cool about yourself"
+                                        style={{ minHeight: '150px', maxHeight: '300px' }}
+                                        rows={2}
+                                        maxLength={255}
+                                        className="mb-3"
+                                        disabled={api.busy}
+                                        value={formData.bio || ""}
+                                        onChange={(e) => setField("bio", e.target.value)}
+                                        isInvalid={!!errors.bio} />
+                                    <Form.Control.Feedback type='invalid'>
+                                        {errors.bio}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                            </Col>
+                        </Row>
+                        <Row className="mb-3">
+                            <Col sm="auto">
+                                <Image roundedCircle
+                                    style={{ backgroundColor: "#000", width: "10vh", height: "10vh" }}
+                                    src={formData.image ? URL.createObjectURL(formData.image) : (userContext.user.image && ("/api/attachments/get?id=" + userContext.user.image))} />
+                            </Col>
+                            <Col>
+                                <Form.Group controlId="formFile" className="mb-3">
+                                    <Form.Label>Upload your photo</Form.Label>
+                                    <Form.Control
+                                        type="file"
+                                        accept="image/png, image/jpeg, image/webp"
+                                        disabled={api.busy}
+                                        onChange={(e) => {
+                                            if (e.target.files && e.target.files.length > 0) {
+                                                setField("image", e.target.files[0])
+                                                setIsImageCrop(true)
+                                            }
+                                        }}
+                                        isInvalid={!!errors.image}
+                                    />
+                                    <Form.Control.Feedback type='invalid'>
+                                        {errors.image}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row className="px-4">
+                            <Button variant="primary" type="button" disabled={api.busy}
+                                onClick={() => processEdit()}>
+                                Save
+                            </Button>
+                        </Row>
+                    </>}
+                {
+                    isImageCrop && <>
+                        <Row className="mb-3">
+                            <Col>
+                                <div
+                                    style={{
+                                        position: "relative",
+                                        height: "30vh",
+                                        width: "100%",
+                                        borderRadius: "10px",
+                                        overflow: "hidden"
+                                    }}>
+                                    <Cropper
+                                        image={URL.createObjectURL(formData.image)}
+                                        crop={crop}
+                                        zoom={zoom}
+                                        aspect={1 / 1}
+                                        onCropChange={setCrop}
+                                        onZoomChange={setZoom}
+                                        onCropComplete={(_, croppedAreaPixels) => {
+                                            setCroppedAreaPixels(croppedAreaPixels);
+                                        }} />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <Col xs="9" className="d-grid">
+                                <Button onClick={OnCropComplete}>Next</Button>
+                            </Col>
+                        </Row>
+                    </>
+                }
             </Container>
         </>
     )
