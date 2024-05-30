c6fd59df3b5b4db5dbbda80379c42ceada869d86
maksp <55075653+maksp86@users.noreply.github.com>
2024-05-30 23:34:55 +0500

    fix logger

--- server/logger.js
+++ server/logger.js
@@ -1,14 +1,40 @@
 const winston = require("winston")
+const fs = require("fs")
+const path = require("path")
+require('winston-daily-rotate-file');
+
+
+let dest = path.join(__dirname, "logs");
+if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest);
+}
+
 
 const logFileFormat = winston.format.combine(
     winston.format.timestamp({}),
+    winston.format.splat(),
     winston.format.json()
 )
 
 const excludeErrorLevel = winston.format((info) => (info.level === 'error') ? false : info);
 
-winston.add(new winston.transports.File({ filename: 'error.log', level: 'error', format: logFileFormat }));
-winston.add(new winston.transports.File({ filename: 'info.log', format: winston.format.combine(excludeErrorLevel(), logFileFormat) }));
+winston.add(new winston.transports.DailyRotateFile({
+    filename: path.join(dest, 'error-%DATE%.log'),
+    datePattern: 'DD-MM-YYYY',
+    maxFiles: '14d',
+    maxSize: '100m',
+    level: 'error',
+    format: logFileFormat
+}));
+
+winston.add(new winston.transports.DailyRotateFile({
+    filename: path.join(dest, 'info-%DATE%.log'),
+    datePattern: 'DD-MM-YYYY',
+    maxFiles: '14d',
+    maxSize: '100m',
+    level: 'info',
+    format: winston.format.combine(excludeErrorLevel(), logFileFormat)
+}));
 
 if (process.env.NODE_ENV !== 'prod') {
     const consoleFormat = winston.format.printf((info) => {
@@ -20,7 +46,8 @@ if (process.env.NODE_ENV !== 'prod') {
                 format: 'YYYY-MM-DD HH:mm:ss'
             }),
             winston.format.splat(),
-            consoleFormat
+            consoleFormat,
+            winston.format.colorize({all: true})
         ),
         level: "debug"
     }));
